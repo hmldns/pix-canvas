b8500877d106bbd0cfec4c318f92aa32de7defb7
Ivan Makarov <ivan_makarov@icloud.com>
2025-08-09 02:17:45 -0700

    Add Discord link to InfoWidget: include new Discord icon and button, update layout for combined icon.

--- apps/frontend/src/components/widgets/InfoWidget.tsx
+++ apps/frontend/src/components/widgets/InfoWidget.tsx
@@ -17,6 +17,13 @@ const InfoWidget: React.FC = () => {
     </svg>
   );
 
+  // Discord icon
+  const discordIcon = (
+    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.211.375-.445.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.956-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.418-2.157 2.418zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.418-2.157 2.418Z" fill="currentColor"/>
+    </svg>
+  );
+
   const widgetContent = (
     <div className="flex flex-col items-center">
       <div className="mb-3 text-center">
@@ -68,6 +75,26 @@ const InfoWidget: React.FC = () => {
           </span>
           <span>Author on X</span>
         </a>
+
+        <a 
+          href="https://discord.gg/nautex" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="
+            flex items-center justify-center
+            px-4 py-2 w-full
+            bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-600 dark:hover:bg-indigo-700
+            text-white
+            rounded-md
+            transition-colors duration-150
+            focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+          "
+        >
+          <span className="mr-2 w-5 h-5">
+            {discordIcon}
+          </span>
+          <span>More On Discord</span>
+        </a>
       </div>
     </div>
   );
@@ -75,8 +102,9 @@ const InfoWidget: React.FC = () => {
   // Combined icon for mobile widget
   const combinedIcon = (
     <div className="flex items-center space-x-1">
-      <span className="w-5 h-5">{githubIcon}</span>
-      <span className="w-5 h-5">{xIcon}</span>
+      <span className="w-4 h-4">{githubIcon}</span>
+      <span className="w-4 h-4">{xIcon}</span>
+      <span className="w-4 h-4">{discordIcon}</span>
     </div>
   );
 
